74c2afe40528f121b20b558b640bfdfad526d488
daynual <72673879+daynual@users.noreply.github.com>
2021-04-08 18:44:02 -0400

    Update Firmware
    -Added Comments
    -Made Calibration faster

--- firmware/SensorDemo.X/main.c
+++ firmware/SensorDemo.X/main.c
@@ -68,6 +68,7 @@ uint16_t sensVal = 0;
 #define upperBound 2047 + 50//2052;
 
 bool runCalibrate;
+bool runSensor;
 uint8_t cmd;
 uint16_t dacIncrement = 1023;
 uint8_t flag;
@@ -89,28 +90,42 @@ int main(void)
     __delay_ms(5000);
     printf("Settling complete.\n\r");
     
+    //Calibrates upon reset
     runCalibrate = true;
     calibrate();
     dacIncrement = 1023;
+    runSensor = true;
     
     while (1)
     { 
         cmd = UART1_RX_NB();
-        if (cmd == '1') {
-            runCalibrate = true;
-            calibrate();
-            dacIncrement = 200;
+        switch(cmd) {
+            case '1': //Calibrates & reads sensor
+                runCalibrate = true;
+                calibrate();
+                dacIncrement = 200;
+                runSensor = true;
+                break;
+            case '2': //Continue sensor reading
+                runSensor = true;
+                break;
+            case '3': //Stop sensor reading
+                runSensor = false;
+                break;
+                
         }
-        
-        
      
-        ADC1Val();
-        printf("%u\n\r", sensVal);
+        //Updates ADC reading and prints through UART
+        if (runSensor) {
+            ADC1Val();
+            printf("%u\n\r", sensVal);
+        }
         
     }
     return 1; 
 }
 
+//Updates ADC reading
 void ADC1Val() {
     AD1CON1bits.DONE = 0;               // Clear DONE bit Before Start Sampling
     AD1CON1bits.SAMP = 1;               // Start Sampling Manually
@@ -119,6 +134,7 @@ void ADC1Val() {
     
 }
 
+//UART Receive Non-Blocking
 char UART1_RX_NB(void) {
     if(U1STAbits.URXDA == 1) {
         return U1RXREG;
@@ -130,79 +146,78 @@ char UART1_RX_NB(void) {
 
 void calibrate(void) {
     while (runCalibrate) {
-        printf("====================================\n\r");
         ADC1Val();
+#ifdef DEBUG
+        printf("====================================\n\r");
         printf("ADC VALUE BEFORE: %d\n\r", sensVal);
         printf("dacValue VALUE BEFORE: %u\n\r", dacVal);
-        
-        if((sensVal <= MINBOUND) || (sensVal >= MAXBOUND)){
-            if(sensVal <= MINBOUND) {
-                dacVal -= dacIncrement;
-                flag = 0; //DAC IS BELOW ADC READING
-                printf("subtracting %u\n\r", dacIncrement);
-            } else if (sensVal >= MAXBOUND) {
-                dacVal += dacIncrement;
-                flag = 1; //DAC IS ABOVE ADC READING
-                printf("adding %u\n\r", dacIncrement);
-            }
+#endif
 
-            SPI_transfer16(0b0000000000000000 | dacVal);
-            
-            ADC1Val();
-            
-            if((sensVal > lowBound) && (sensVal < upperBound)) {
-                runCalibrate = false; // BREAK WHILE LOOP
-                printf("WITHIN BOUNDS!!!\n\r");
-            } else if (dacIncrement == 0) {
-                runCalibrate = false;
-                printf("dacIncrement == 0!\n\r");
-            }
-
-            
-            uint16_t testVal = 0b0000000000000000 | dacVal;
-            printf("dacVal: %u\n\r", testVal);
-            
-            printf("ADC VALUE HALFWAY: %u\n\r", sensVal);
-            
-            
-            
-            if((sensVal <= MINBOUND) && (flag == 1)) {
-                dacIncrement = (uint16_t)(dacIncrement / 2) + 1;
-                printf("flipped!!\n\r");
-            } else if ((sensVal >= MAXBOUND) && (flag == 0)) {
-                dacIncrement = (uint16_t)(dacIncrement / 2) + 1;
-                printf("flipped!!\n\r");
-            }  
-            
-        } else if(((sensVal < lowBound) && (sensVal > MINBOUND)) || ((sensVal > upperBound) && (sensVal < MAXBOUND))) {
-            printf("222222222222222222222222222222\n\r");
-            if(sensVal < lowBound) {
-                dacVal -= 1;
-                printf("adding 1\n\r");
-                SPI_transfer16(0b0000000000000000 | dacVal);
-            } else if (sensVal > upperBound) {
-                dacVal += 1;
-                printf("subtracting 1\n\r");
-                SPI_transfer16(0b0000000000000000 | dacVal);
-            }
-            
-            //add stuff here
-            
-        } else {
-            runCalibrate = false;
+        //if the sensor reading is above/below setpoint, change DAC output
+        if(sensVal <= lowBound) {
+            dacVal -= dacIncrement;
+            flag = 0; //DAC IS BELOW ADC READING
+#ifdef DEBUG
+            printf("subtracting %u\n\r", dacIncrement);
+#endif
+        } else if (sensVal >= upperBound) {
+            dacVal += dacIncrement;
+            flag = 1; //DAC IS ABOVE ADC READING
+#ifdef DEBUG
+            printf("adding %u\n\r", dacIncrement);
+#endif
         }
+
+        SPI_transfer16(dacVal);
+        ADC1Val();
+
+        //Checks if ADC reading is within range
+        if((sensVal >= lowBound) && (sensVal <= upperBound)) {
+            runCalibrate = false; // BREAK WHILE LOOP
+#ifdef DEBUG
+            printf("WITHIN BOUNDS!!!\n\r");
+#endif
+        } else if (dacIncrement == 0) {
+            dacIncrement += 1;
+#ifdef DEBUG
+            printf("dacIncrement == 0!\n\r");
+#endif
+        }
+
+#ifdef DEBUG
+        uint16_t testVal = 0b0000000000000000 | dacVal;
+        printf("dacVal: %u\n\r", testVal);
+        
+        printf("ADC VALUE HALFWAY: %u\n\r", sensVal);
+#endif
+
+
+        //Checks if DAC crossed the setpoint range. If so, decrease the increment.
+        if((sensVal <= lowBound) && (flag == 1)) {
+            dacIncrement = (uint16_t)(dacIncrement / 2);
+#ifdef DEBUG
+            printf("flipped!!\n\r");
+#endif
+        } else if ((sensVal >= upperBound) && (flag == 0)) {
+            dacIncrement = (uint16_t)(dacIncrement / 2);
+#ifdef DEBUG
+            printf("flipped!!\n\r");
+#endif
+        }  
         
         ADC1Val();
-        printf("ADC VALUE AFTER: %d\n\r", sensVal);
+        printf("Calibrating... %u\n\r", sensVal);
     }
 }
 
+//Send 16bit data through SPI bus
 void SPI_transfer16(uint16_t input) {
     CS1_SetLow();
     SPI2_Exchange16bit(input);
     CS1_SetHigh();
     __delay_ms(1);
 }
+
 /**
  End of File
 */
--- firmware/SensorDemo.X/mcc_generated_files/mcc.h
+++ firmware/SensorDemo.X/mcc_generated_files/mcc.h
@@ -53,13 +53,13 @@
 #include "system_types.h"
 #include "reset.h"
 
-#include "adc1.h"
 #include "interrupt_manager.h"
 #include "traps.h"
-#include "spi2.h"
-#include "uart1.h"
-#include "reset.h"
 #include "watchdog.h"
+#include "uart1.h"
+#include "spi2.h"
+#include "adc1.h"
+#include "reset.h"
 
 #warning "This file will be removed in future MCC releases. Use system.h instead."
 
